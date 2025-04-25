3682cf6fde1a0b2f414456438006614dc89acadb
dev-rfgul <rfgul587@gmail.com>
2025-04-25 08:59:35 +0500

    completed anagram problem

--- easy/anagram.js
+++ easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+  const normalize = (str) => str.toLowerCase().replace(/\s/g, '');
 
+  const normalizedStr1 = normalize(str1);
+  const normalizedStr2 = normalize(str2);
+
+  console.log(`Normalized: ${normalizedStr1} ${normalizedStr2}`);
+
+  if (normalizedStr1.length !== normalizedStr2.length) return false;
+
+  return normalizedStr1.split('').sort().join('') === normalizedStr2.split('').sort().join('');
 }
 
 module.exports = isAnagram;
+
+
+
